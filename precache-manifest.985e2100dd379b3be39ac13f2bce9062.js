1e41749d7e83e94833e746448f31e417c2d37910
Abdullah Nassar <abdullahnassar128@gmail.com>
2019-08-26 20:39:21 +0300

    dist

---
+++ precache-manifest.985e2100dd379b3be39ac13f2bce9062.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "83f85375cbfadf9529c1",
+    "url": "/expensive-time/css/app.1fd3b4cd.css"
+  },
+  {
+    "revision": "83f85375cbfadf9529c1",
+    "url": "/expensive-time/js/app.db4c1e5d.js"
+  },
+  {
+    "revision": "bff8a4b27e6317335cbd",
+    "url": "/expensive-time/css/chunk-vendors.7f89301a.css"
+  },
+  {
+    "revision": "bff8a4b27e6317335cbd",
+    "url": "/expensive-time/js/chunk-vendors.e3e4ab85.js"
+  },
+  {
+    "revision": "aab6e1f58295b51086c6759eaff3454f",
+    "url": "/expensive-time/index.html"
+  },
+  {
+    "revision": "e8e4d9e04f115e4db44630b53d52e910",
+    "url": "/expensive-time/Capture2.JPG"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/expensive-time/robots.txt"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/expensive-time/precache-manifest.985e2100dd379b3be39ac13f2bce9062.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "expensive-time"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
